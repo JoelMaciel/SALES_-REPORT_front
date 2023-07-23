e5725d6192835335e3155b4c21d30655445694fd
Joel Maciel <soldado25461@gmail.com>
2023-07-23 10:27:13 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Sales Report</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
